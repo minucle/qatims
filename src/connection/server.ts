c0b78c4d714f2c9f4613595b31e57888c515f5b5
terthesz <60745997+terthesz@users.noreply.github.com>
2022-07-27 20:11:06 +0200

    🌟 Added TODOS to the Server class (will implement tomorrow)

--- src/connection/server.ts
+++ src/connection/server.ts
@@ -1,11 +1,65 @@
-import io from "socket.io";
+import io from 'socket.io';
 
+/**
+ * This class handles everything related to the TCP server.
+ */
 class Server {
+  /**
+   * The socket.io Server instance.
+   *
+   * @type {io.Server}
+   * @memberof Server
+   * @private
+   */
   private io_server: io.Server;
+  /**
+   * The server port.
+   *
+   * @type {number}
+   * @memberof Server
+   * @private
+   */
   private port: number;
 
+  /**
+   * Server class constructor.
+   *
+   * @param {number} port The server port.
+   * @memberof Server
+   * @constructor
+   * @throws {Error} If the port is not valid.
+   */
   constructor(port: number) {
-    if (!parseInt(port + "")) throw new Error("Port must be a number");
+    // Validate port
+    if (!parseInt(port + '') || port < 0) throw new Error('❌ Invalid port.');
     port = port || 8080;
+
+    this.port = port;
+
+    // Create the socket.io server
+    this.io_server = new io.Server(this.port);
+  }
+
+  /**
+   * Handles new connections.
+   *
+   * @param socket The connecting socket.
+   */
+  private _onConnection(socket: io.Socket) {
+    // Parse data from connection.
+    const { identifier } = socket.handshake.query;
+    const address = socket.handshake.address;
+
+    // Validate connection.
+    // TODO: IP Validation
+    // TODO: Identifier Validation (if valid, if has permission and if not connected. If not - ban IP and alert staff.)
+
+    // Log connection
+    console.log(`📡 New connection: ${identifier}`);
+    // TODO: Local logger
+
+    // Handle disconnection and errors.
+    // TODO: Listen to disconnection and error events.
+    // TODO: Local logger
   }
 }
